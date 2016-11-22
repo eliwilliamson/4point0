44f6a2ee7905558a350b3fbee6f20012e3c7a234
eliwilliamson <eliwilliamson@gmail.com>
2016-11-21 21:46:04 -0500

    fix thank you and form

--- scripts/application.js
+++ scripts/application.js
@@ -1,21 +1,3 @@
-// instagram feed
-$(function() {
-  'use strict';
-
-  if ($('body').hasClass('lifestream')) {
-    var feed = new Instafeed({
-      get: 'user',
-      userId: 5426996,
-      template: '<article class="col-md-3 instagram-image"><img src="{{image}}" alt="instagram picture"></article>',
-      clientId: '6730b5366a4e4e918d903d3e9314cdf1',
-      accessToken: '5426996.1677ed0.da901553a3c944dbbcdb1cb907171ef2',
-      template: '<article class="col-md-3 instagram-image"><img src="{{image}}" alt="instagram picture"></article>',
-      resolution: 'standard_resolution',
-      limit: 32
-    });
-    return feed.run();
-  }
-});
 
 
 // cta particles
@@ -149,3 +131,23 @@ $(function() {
     };
   }
 }).call(this);
+
+
+// instagram feed
+$(function() {
+  'use strict';
+
+  if ($('body').hasClass('lifestream')) {
+    var feed = new Instafeed({
+      get: 'user',
+      userId: 5426996,
+      template: '<article class="col-md-3 instagram-image"><img src="{{image}}" alt="instagram picture"></article>',
+      clientId: '6730b5366a4e4e918d903d3e9314cdf1',
+      accessToken: '5426996.1677ed0.da901553a3c944dbbcdb1cb907171ef2',
+      template: '<article class="col-md-3 instagram-image"><img src="{{image}}" alt="instagram picture"></article>',
+      resolution: 'standard_resolution',
+      limit: 32
+    });
+    return feed.run();
+  }
+});
